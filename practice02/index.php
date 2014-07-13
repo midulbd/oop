73fa9d0c145048cae01df7ee7885585129234706
towfiqpiash <towfiqpiash@gmail.com>
2014-07-13 13:07:05 +0600

    Style updated

--- practice02/index.php
+++ practice02/index.php
@@ -5,33 +5,48 @@
 </head>
 <body>
 	<form action="index.php" method="GET">
-		<label>Principal Amount</label>
-		<input type="text" name="principalAmountText"><br/>
+		<table align="center" style="background: lightgrey">
+			<tr>
+				<td colspan="2">
+					<h2 style="background: yellowgreen; text-align:center">Simple Interest Calculator</h2>
+				</td>
+			</tr>
+			<tr>
+				<td>Principal Amount</td>
+				<td><input type="text" name="principalAmountText"></td>
+			</tr>
+			<tr>
+				<td>Annual Interst Rate</td>
+				<td><input type="text" name="annualInterestRateText"> %</td>
+			</tr>
+			<tr>
+				<td>Time Period</td>
+				<td><input type="text" name="timePeriodText"> year</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td><input type="submit" name="calculateButton" value="Calculate" width="100"></td>
+			</tr>
+			<tr>
+				<td colspan="2" style="background:white">
+					<?php
+					require_once 'interestcalculator.php';
+					if(isset($_GET['calculateButton'])){
 
-		<label>Annual Interst Rate</label>
-		<input type="text" name="annualInterestRateText"> %<br/>
+						$interest = new InterestCalculator();
+						
+						$interest->principal_amount = $_GET['principalAmountText'];
+						$interest->annual_interest_rate = $_GET['annualInterestRateText'];
+						$interest->time_period = $_GET['timePeriodText'];
 
-		<label>Time Period</label>
-		<input type="text" name="timePeriodText"> year<br/>
+						$Result = $interest->get_total_amount();
+						echo "<h2> Result: ".$Result."</h2>";	
+						}
 
-		<input type="submit" name="calculateButton" value="Calculate">
+					?>
+				</td>
+			</tr>
+		</table>
 	</form>
-	
-	<?php
-		require_once 'interestcalculator.php';
-		if(isset($_GET['calculateButton'])){
-
-			$interest = new InterestCalculator();
-			
-			$interest->principal_amount = $_GET['principalAmountText'];
-			$interest->annual_interest_rate = $_GET['annualInterestRateText'];
-			$interest->time_period = $_GET['timePeriodText'];
-
-			$Result = $interest->get_total_amount();
-			echo "<br/>";
-			echo "Result: ".$Result;	
-		}
-
-	?>
 </body>
 </html>
