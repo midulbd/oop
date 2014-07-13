c9588716d51a30e3c084ed75a8cf265ed2f09ec0
towfiqpiash <towfiqpiash@gmail.com>
2014-07-13 10:35:33 +0600

    - Employee Input form added - object used to access class

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>User Input Form</title>
+</head>
+<body>
+	<form action="index.php" method="GET">
+		<input type="text" name="idText"><br/>
+		<input type="text" name="nameText"><br/>
+		<input type="text" name="salaryText"><br/>
+		<input type="submit" name="submitButton" value="Submit">
+	</form>
+	<?php
+		require_once 'officeemployee.php';
+		if(isset($_GET['submitButton'])){
+
+			$an_employee = new OfficeEmployee();
+
+			$an_employee->id = $_GET['idText']; //$_POST or $_GET has the value in dictionary(Key-value pair) format
+			$an_employee->name = $_GET['nameText'];
+			$an_employee->salary = $_GET['salaryText'];
+
+			echo $an_employee->id.' '.$an_employee->name.' '.$an_employee->salary;	
+		}
+
+	?>
+</body>
+</html>
